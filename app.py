ab8f3283786d18c4d5a1a937e82f29077289c059
VIKRA2023 <122616397+VIKRA2023@users.noreply.github.com>
2023-07-19 08:30:11 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask,render_template , request , jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home_page():
+    return render_template('index.html')
+
+
+@app.route('/postman_data',methods = ['POST'])
+def math_operation1():
+    if(request.method == 'POST'):
+        ops = request.json['operation']
+        num1 = int(request.json['num1'])
+        num2 = int(request.json['num2'])
+        if ops == 'add':
+            r = num1+num2
+            result = "The sum of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
+        if ops == 'subtract':
+            r = num1-num2
+            result = "The subtract of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
+        if ops == 'multiply':
+            r = num1*num2
+            result = "The multiply of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
+        if ops == 'divide':
+            r = num1/num2
+            result = "The divide of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
+            
+        return jsonify(result)
+
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0")
